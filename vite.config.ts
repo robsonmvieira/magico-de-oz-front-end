9a11a73f9b515e48fc82e125e32f16c13e0d6701
Robson Maia <robsonmvieira@gmail.com>
2026-01-02 15:48:56 +0000

    fix: :wrench: temporarily remove router-plugin to fix vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
@@ -12,13 +11,6 @@ export default defineConfig({
 		css: true
 	},
 	plugins: [
-		tanstackRouter({
-			target: 'react',
-			autoCodeSplitting: true,
-			routesDirectory: './src/pages',
-			semicolons: false,
-			quoteStyle: 'single'
-		}),
 		react(),
 		tailwindcss()
 	],
